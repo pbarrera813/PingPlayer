df91f8b10d8af946025dd83b58065281d1f38bff
pbarrera813 <pbarrera813@gmail.com>
2026-04-05 21:48:31 -0600

    chore(neoforge): add throttled debug logging for tab display rendering

--- pingplayer-neoforge/src/main/java/player/ping/mixin/ServerPlayerMixin.java
+++ pingplayer-neoforge/src/main/java/player/ping/mixin/ServerPlayerMixin.java
@@ -7,11 +7,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import player.ping.NeoForgeCompat;
 import player.ping.PingSettings;
 import player.ping.PingUtils;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin {
+
+    private static final long DEBUG_TAB_ENTRY_THROTTLE_MS = 3000L;
 
     @Inject(method = "getTabListDisplayName", at = @At("HEAD"), cancellable = true)
     private void pingplayer$getTabListDisplayName(CallbackInfoReturnable<Component> cir) {
@@ -21,12 +24,14 @@ public abstract class ServerPlayerMixin {
 
         ServerPlayer self = (ServerPlayer) (Object) this;
         int ping = self.connection.latency();
-        String playerName = self.getGameProfile().getName();
+        String playerName = NeoForgeCompat.getProfileName(self);
         ChatFormatting color = PingUtils.getPingColor(ping);
 
         Component formattedTabName = Component.literal(playerName)
                 .append(Component.literal(" [" + ping + " ms]").withStyle(style -> style.withColor(color)));
 
+        NeoForgeCompat.debugThrottled("tab/mixin", "tab-entry-" + playerName.toLowerCase(), DEBUG_TAB_ENTRY_THROTTLE_MS,
+                "Generated tab display for '%s': ping=%dms color=%s", playerName, ping, color.name());
         cir.setReturnValue(formattedTabName);
     }
 }
